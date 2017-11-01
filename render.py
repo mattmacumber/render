5fed750c09568fdb89e7d0b9af4c25bf53ae22fb
Matt Macumber <matt.macumber@gmail.com>
2017-10-31 22:35:48 -0700

    Threads make it slower

--- render.py
+++ render.py
@@ -1,26 +1,35 @@
 import math
+import multiprocessing
 import sys
+import threading
 
-from random import random as rand
 from PIL import Image
+from random import random as rand
+from time import time, sleep
 
-#render_profile = "ONE_MINUTE"
-render_profile = "Infinity"
+render_profile = "QUICK_SMALL"
+#render_profile = "PRETTY_SMALL"
 #render_profile = "SINGLE_PASS"
+#render_profile = "Infinity"
 
-if render_profile == "ONE_MINUTE":
+if render_profile == "QUICK_SMALL":
     WIDTH = 1920/4
     HEIGHT = 1024/4
 
     RAYS_PER_PIXEL = 4
-    MAX_BOUNCE = 8
+    MAX_BOUNCE = 4 
 elif render_profile == "SINGLE_PASS":
-    # ~ 200 seconds
     WIDTH = 1920
     HEIGHT = 1024
 
     RAYS_PER_PIXEL = 1
-    MAX_BOUNCE = 8
+    MAX_BOUNCE = 4
+elif render_profile == "PRETTY_SMALL":
+    WIDTH = 1920/4
+    HEIGHT = 1024/4
+
+    RAYS_PER_PIXEL = 10
+    MAX_BOUNCE = 4
 else:
     WIDTH = 1920
     HEIGHT = 1024
@@ -140,86 +149,155 @@ class Plane(object):
         self.material = material
 
 class Material(object):
-    def __init__(self, emit_color=V3(), refl_color=V3(), scatter=0.5):
+    def __init__(self, emit_color=V3(), refl_color=V3(), scatter=0.0):
         self.emit_color = emit_color
         self.refl_color = refl_color
         self.scatter = scatter
 
-def RayCast(world, ray_origin, ray_dir):
-    ret = V3(0, 0, 0)
-    attenuation = V3(1, 1, 1)
+class WorkQueue(object):
+    def __init__(self):
+        self.work_orders = list()
+        self.bounces_computed = 0
+        self.tiles_rendered = 0
+        self.total_tile_count = 0
 
-    min_hit_distance = 0.001
-    tolerance = 0.0001
+def render_tile(queue):
 
-    for ray_count in xrange(MAX_BOUNCE):
-        hit_dist = 10**100
+    while len(queue.work_orders) > 0:
+        work_order = queue.work_orders.pop()
+        world, image, x_min, y_min, x_max, y_max = work_order
 
-        hit_material = None
+        camera_pos = V3(0, -10, 1)
+        camera_z = NoZ(camera_pos)
+        camera_x = NoZ(Cross(camera_z, V3(0, 0, 1)))
+        camera_y = NoZ(Cross(camera_z, camera_x))
 
-        next_normal = None
+        image_width = image.size[0]
+        image_height = image.size[1]
 
-        for plane in world.planes:
-            denom = Inner(plane.n, ray_dir)
-            if denom < -tolerance or tolerance < denom:
-                t = (- plane.d - Inner(plane.n, ray_origin))/denom
-                if 0 < t < hit_dist:
-                    hit_dist = t
-                    hit_material = plane.material
-
-                    next_normal = plane.n
-
-        for sphere in world.spheres:
-            sphere_origin_translate = ray_origin - sphere.center
-
-            a = Inner(ray_dir, ray_dir)
-            b = 2.0*Inner(ray_dir, sphere_origin_translate)
-            c = Inner(sphere_origin_translate, sphere_origin_translate) - sphere.radius**2
-
-            denom = 2*a
-            sqrd = b*b-4*a*c
-            sqrd = max(0, sqrd)
-            root_term = math.sqrt(sqrd)
-            if root_term > tolerance:
-
-                pos = (-b + root_term) / denom
-                neg = (-b - root_term) / denom
-
-                t = pos
-                if min_hit_distance < neg < pos:
-                    t = neg
-
-                if min_hit_distance < t < hit_dist:
-                    hit_dist = t
-                    hit_material = sphere.material
-
-                    next_normal = NoZ(t*ray_dir+sphere_origin_translate)
-
-        if hit_material is not None:
-            ret += Hadamard(attenuation, hit_material.emit_color)
-            
-            cos_atten = Inner(ray_dir*-1, next_normal)
-            cos_atten = max(0, cos_atten)
-
-            attenuation = Hadamard(attenuation, cos_atten * hit_material.refl_color)
-
-            ray_origin += hit_dist * ray_dir
-
-            pure_bounce = ray_dir - 2*Inner(ray_dir, next_normal)*next_normal
-            random_bounce = NoZ(next_normal + V3(rand()*2-1, rand()*2-1, rand()*2-1))
-
-            ray_dir = NoZ(lerp(random_bounce, hit_material.scatter, pure_bounce))
-
+        film_dist = 1.0
+        film_w = 1.0
+        film_h = 1.0
+        
+        if image_width > image_height:
+            film_h = film_w * image_height/image_width
         else:
-            ret += Hadamard(attenuation, world.materials[0].emit_color)
-            break 
+            film_w = film_h * image_width/image_height 
+       
+        film_half_w = film_w/2.0
+        film_half_h = film_h/2.0
 
-    return ret
+        film_center = camera_pos - film_dist*camera_z
+
+        pix_width = 0.5 / image_width
+        pix_height = 0.5 / image_height
+
+        pixels = image.load()
+
+        bounces_computed = 0
+                
+        for x in xrange(x_min, x_max): 
+            film_x = -1.0+2.0*x/image_width;
+            for y in range(y_min, y_max):
+                film_y = -1.0+2.0*y/image_height;
+
+                color = V3()
+                fraction = 1.0/RAYS_PER_PIXEL
+                for _ in xrange(RAYS_PER_PIXEL):
+
+                    off_x = film_x + (rand()*2-1)*pix_width
+                    off_y = film_y + (rand()*2-1)*pix_height
+
+                    film_p = film_center + off_x*film_half_w*camera_x + off_y*film_half_h*camera_y
+
+                    ray_origin = camera_pos
+                    ray_dir = NoZ(film_p - camera_pos)
+
+                    #color += RayCast(world, ray_origin, ray_dir)*fraction
+                    result = V3(0, 0, 0)
+                    attenuation = V3(1, 1, 1)
+                
+                    min_hit_distance = 0.001
+                    tolerance = 0.0001
+                
+                    for bounce_count in xrange(MAX_BOUNCE):
+                        hit_dist = 10**100
+                
+                        hit_material = None
+                
+                        next_normal = None
+                
+                        bounces_computed += 1
+                
+                        for plane in world.planes:
+                            denom = Inner(plane.n, ray_dir)
+                            if denom < -tolerance or tolerance < denom:
+                                t = (- plane.d - Inner(plane.n, ray_origin))/denom
+                                if 0 < t < hit_dist:
+                                    hit_dist = t
+                                    hit_material = plane.material
+                
+                                    next_normal = plane.n
+                
+                        for sphere in world.spheres:
+                            sphere_origin_translate = ray_origin - sphere.center
+                
+                            a = Inner(ray_dir, ray_dir)
+                            b = 2.0*Inner(ray_dir, sphere_origin_translate)
+                            c = Inner(sphere_origin_translate, sphere_origin_translate) - sphere.radius**2
+                
+                            denom = 2*a
+                            sqrd = b*b-4*a*c
+                            sqrd = max(0, sqrd)
+                            root_term = math.sqrt(sqrd)
+                            if root_term > tolerance:
+                
+                                pos = (-b + root_term) / denom
+                                neg = (-b - root_term) / denom
+                
+                                t = pos
+                                if min_hit_distance < neg < pos:
+                                    t = neg
+                
+                                if min_hit_distance < t < hit_dist:
+                                    hit_dist = t
+                                    hit_material = sphere.material
+                
+                                    next_normal = NoZ(t*ray_dir+sphere_origin_translate)
+                
+                        if hit_material is not None:
+                            result += Hadamard(attenuation, hit_material.emit_color)
+                            
+                            cos_atten = Inner(ray_dir*-1, next_normal)
+                            cos_atten = max(0, cos_atten)
+                
+                            attenuation = Hadamard(attenuation, cos_atten * hit_material.refl_color)
+                
+                            ray_origin += hit_dist * ray_dir
+                
+                            pure_bounce = ray_dir - 2*Inner(ray_dir, next_normal)*next_normal
+                            random_bounce = NoZ(next_normal + V3(rand()*2-1, rand()*2-1, rand()*2-1))
+                
+                            ray_dir = NoZ(lerp(random_bounce, hit_material.scatter, pure_bounce))
+                
+                        else:
+                            result += Hadamard(attenuation, world.materials[0].emit_color)
+                            break 
+                
+                
+                    color += result*fraction
+
+                pixel = V3(LinearToRGB(color.x), LinearToRGB(color.y), LinearToRGB(color.z))
+                pixel = Linear1ToRGB255(pixel)
+                pixels[image_width -x -1,y] = pixel.tuple()
+
+        queue.bounces_computed += bounces_computed
+        queue.tiles_rendered += 1
+        print "\rRaycasting %1.2f %%...   " % (100.0*queue.tiles_rendered/(queue.total_tile_count)),
+        sys.stdout.flush()
 
 def render(img):
-    image_width = img.size[0]
-    image_height = img.size[1]
-
+    
     world = World()
 
     world.materials.append( Material( V3(0.3, 0.4, 0.5), V3(0, 0, 0), 0.0 ))
@@ -238,57 +316,45 @@ def render(img):
     world.spheres.append(Sphere(V3(-2, -1, 2), 1.0, world.materials[4]))
     world.spheres.append(Sphere(V3(1, -1, 3), 1.0, world.materials[5]))
     world.spheres.append(Sphere(V3(-2, 3, 0), 2.0, world.materials[6]))
-
-    camera_pos = V3(0, -10, 1)
-    camera_z = NoZ(camera_pos)
-    camera_x = NoZ(Cross(camera_z, V3(0, 0, 1)))
-    camera_y = NoZ(Cross(camera_z, camera_x))
-
-    film_dist = 1.0
-    film_w = 1.0
-    film_h = 1.0
     
-    if image_width > image_height:
-        film_h = film_w * image_height/image_width
-    else:
-        film_w = film_h * image_width/image_height 
-   
-    film_half_w = film_w/2.0
-    film_half_h = film_h/2.0
+    start_time = time()
 
-    film_center = camera_pos - film_dist*camera_z
+    core_count = multiprocessing.cpu_count()
+    tile_width = 256
+    tile_height = tile_width
+    tile_count_x = (img.size[0] + tile_width -1) / tile_width
+    tile_count_y = (img.size[1] + tile_width -1) / tile_height
+    
+    print "Chunking: %d cores with %d %dx%d (%dk/tile) tiles" % (core_count, tile_count_x*tile_count_y, tile_width, tile_height, tile_width*tile_height*4/1024 )
 
-    pix_width = 0.5 / image_width
-    pix_height = 0.5 / image_height
+    queue = WorkQueue()
+    queue.total_tile_count = tile_count_x * tile_count_y
 
-    pixels = img.load()
+    for tile_x in xrange(tile_count_x):
+        min_x = tile_x*tile_width
+        max_x = min(img.size[0], min_x + tile_width)
+        for tile_y in xrange(tile_count_y):
+            min_y = tile_y*tile_height
+            max_y = min(img.size[1], min_y + tile_height)
+            
+            queue.work_orders.append( (world, img, min_x, min_y, max_x, max_y) )
 
-    for x in xrange(image_width): 
-        film_x = -1.0+2.0*x/image_width;
-        for y in range(image_height):
-            film_y = -1.0+2.0*y/image_height;
+    threads = list()
+    for core in xrange(core_count):
+        t = threading.Thread(target=render_tile, args=(queue,))
+        t.start()
+        threads.append(t)
 
-            color = V3()
-            fraction = 1.0/RAYS_PER_PIXEL
-            for _ in xrange(RAYS_PER_PIXEL):
+    for thread in threads:
+        thread.join()
 
-                off_x = film_x + (rand()*2-1)*pix_width
-                off_y = film_y + (rand()*2-1)*pix_height
+    end_time = time()
+    casting_time = (end_time - start_time)*1000
 
-                film_p = film_center + off_x*film_half_w*camera_x + off_y*film_half_h*camera_y
-
-                ray_origin = camera_pos
-                ray_dir = NoZ(film_p - camera_pos)
-
-                color += RayCast(world, ray_origin, ray_dir)*fraction
-
-            pixel = V3(LinearToRGB(color.x), LinearToRGB(color.y), LinearToRGB(color.z))
-            pixel = Linear1ToRGB255(pixel)
-            pixels[image_width-x-1,y] = pixel.tuple()
-
-        #if x % int(image_width/100) == 0:
-        print "\rRaycasting %s %%...   " % (100.0*x/image_width),
-        sys.stdout.flush()
+    print "\n"
+    print "Raycasting time: %dms" % casting_time
+    print "Total bounces: %d" % queue.bounces_computed
+    print "Performance: %fms/bounce" % (casting_time / (queue.bounces_computed+0.01))
 
 def main():
     
